32e97259374dc5c63fb7297485cf8af883288d7e
Silas Eduardo <silasn1@live.com>
2024-11-04 11:45:52 -0300

    feat: criação dos métodos de crud de contrato e funcionários

--- services/crud.py
+++ services/crud.py
@@ -53,7 +53,82 @@ def delete_department(id_departamento):
         print(f"Erro ao deletar departamento: {e}")
     finally:
         conn.close()
+ 
+ 
+# CRUD Contrato
+def read_contrato():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(current_dir, '../ponto.db')
+
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('SELECT * FROM contrato')
+        rows = cursor.fetchall()
+    except sqlite3.OperationalError as e:
+        print(f"Erro ao ler tipos de contrato: {e}")
+    finally:
+         conn.close()
+
+    return rows
+
+
+# CRUD Funcionário
+def create_funcionario(funcionario, id_setor, id_contrato):
+    # Caminho do banco de dados
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(current_dir, '../ponto.db')
+
+    # Conexão ao banco de dados
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # Execução da query SQL
+    try:
+        cursor.execute('INSERT INTO funcionario (funcionario, contrato, departamento) VALUES (?, ?, ?)', (funcionario, id_contrato, id_setor))
+        conn.commit()
+    except sqlite3.OperationalError as e:
+        print(f"Erro ao inserir funcionário: {e}")
+    finally:
+        conn.close()     
+
+def read_funcionarios():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(current_dir, '../ponto.db')
+
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('SELECT * FROM funcionario')
+        rows = cursor.fetchall()
+    except sqlite3.OperationalError as e:
+        print(f"Erro ao ler funcionários: {e}")
+    finally:
+         conn.close()
+
+    return rows
+
+def delete_funcionario(id_funcionario):
+    # Caminho do banco de dados
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(current_dir, '../ponto.db')
+
+    # Conexão ao banco de dados
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # Execução da query SQL
+    try:
+        cursor.execute('DELETE FROM funcionario WHERE id = ?', (id_funcionario,))
+        conn.commit()
+    except sqlite3.OperationalError as e:
+        print(f"Erro ao inserir funcionário: {e}")
+    finally:
+        conn.close()    
         
+               
 # CRUD Ponto
 def create_register(data, setor, tipo_funcionario, nome_funcionario, hora_entrada, hora_saida, observacao):
     conn = sqlite3.connect('../ponto.db')
